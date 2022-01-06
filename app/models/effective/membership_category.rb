3f7e7685d7269027cbb0ce9cd9ad044c036cbf6f
Matt Riemer <matthew@codeandeffect.com>
2022-01-06 10:50:45 -0700

    Add editable price field

--- app/models/effective/membership_category.rb
+++ app/models/effective/membership_category.rb
@@ -3,8 +3,10 @@ module Effective
     belongs_to :category, polymorphic: true
     belongs_to :membership, polymorphic: true
 
+    log_changes(to: :membership)
+
     def to_s
-      'membership category'
+      category&.to_s || 'membership category'
     end
 
   end
