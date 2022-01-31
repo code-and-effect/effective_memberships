6012d19360c8fbde100c12f032180082a850f9e4
Matt Riemer <matthew@codeandeffect.com>
2022-01-31 10:31:55 -0700

    work with newer effective resources

--- app/controllers/effective/applicants_controller.rb
+++ app/controllers/effective/applicants_controller.rb
@@ -6,29 +6,5 @@ module Effective
 
     resource_scope -> { EffectiveMemberships.Applicant.deep.where(owner: current_user.effective_memberships_owners) }
 
-    # Allow only 1 in-progress application at a time
-    before_action(only: [:new, :show], unless: -> { resource&.done? }) do
-      existing = resource_scope.in_progress.where.not(id: resource).first
-
-      if existing.present?
-        flash[:success] = "You have been redirected to your existing in progress application"
-        redirect_to effective_memberships.applicant_build_path(existing, existing.next_step)
-      end
-    end
-
-    after_save do
-      flash.now[:success] = ''
-    end
-
-    private
-
-    def permitted_params
-      params.require(:applicant).permit!.except(
-        :owner_id, :owner_type, :status, :status_steps, :wizard_steps,
-        :submitted_at, :completed_at, :reviewed_at, :approved_at,
-        :declined_at, :declined_reason, :created_at, :updated_at
-      )
-    end
-
   end
 end
--- app/controllers/effective/fee_payments_controller.rb
+++ app/controllers/effective/fee_payments_controller.rb
@@ -6,27 +6,5 @@ module Effective
 
     resource_scope -> { EffectiveMemberships.FeePayment.deep.where(owner: current_user.effective_memberships_owners) }
 
-    # Allow only 1 in-progress fee payment at a time
-    before_action(only: [:new, :show], unless: -> { resource&.done? }) do
-      existing = resource_scope.in_progress.where.not(id: resource).first
-
-      if existing.present?
-        flash[:success] = "You have been redirected to your existing in progress fee payment"
-        redirect_to effective_memberships.fee_payment_build_path(existing, existing.next_step)
-      end
-    end
-
-    after_save do
-      flash.now[:success] = ''
-    end
-
-    private
-
-    def permitted_params
-      params.require(:fee_payment).permit!.except(
-        :status, :status_steps, :wizard_steps, :submitted_at
-      )
-    end
-
   end
 end
