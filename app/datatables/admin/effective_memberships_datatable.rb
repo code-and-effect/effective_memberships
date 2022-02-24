452e73200d50206700467cc44eb5311f02acb6b1
Matt Riemer <matthew@codeandeffect.com>
2022-02-24 09:28:25 -0700

    Add email, phone and address info to Admin memberships index

--- app/datatables/admin/effective_memberships_datatable.rb
+++ app/datatables/admin/effective_memberships_datatable.rb
@@ -24,11 +24,48 @@ module Admin
       col :bad_standing_admin, visible: false
       col :bad_standing_reason, visible: false
 
+      col :email do |membership|
+        email = membership.owner.try(:email)
+        mail_to(email) if email.present?
+      end
+
+      col :phone do |membership|
+        membership.owner.try(:phone) || membership.owner.try(:home_phone) || membership.owner.try(:cell_phone)
+      end
+
+      col :address do |membership|
+        membership.owner.try(:addresses).try(:first).try(:to_html)
+      end
+
+      col :address1, visible: false do |membership|
+        membership.owner.try(:addresses).try(:first).try(:address1)
+      end
+
+      col :address2, visible: false do |membership|
+        membership.owner.try(:addresses).try(:first).try(:address2)
+      end
+
+      col :city, visible: false do |membership|
+        membership.owner.try(:addresses).try(:first).try(:city)
+      end
+
+      col :province, visible: false do |membership|
+        membership.owner.try(:addresses).try(:first).try(:province)
+      end
+
+      col :country, visible: false do |membership|
+        membership.owner.try(:addresses).try(:first).try(:country)
+      end
+
+      col :postal_code, visible: false do |membership|
+        membership.owner.try(:addresses).try(:first).try(:postal_code)
+      end
+
       actions_col
     end
 
     collection do
-      memberships = Effective::Membership.deep.all.includes(:owner)
+      memberships = Effective::Membership.deep.all.includes(owner: :addresses)
 
       raise('expected an owner_id, not user_id') if attributes[:user_id].present?
 
