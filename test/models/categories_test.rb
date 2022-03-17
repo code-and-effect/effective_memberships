062bf8460e6aa0058f7baa2991f1d436104f2ec3
Matt Riemer <matthew@codeandeffect.com>
2022-03-17 13:20:46 -0600

    fix tests

--- test/models/categories_test.rb
+++ test/models/categories_test.rb
@@ -8,6 +8,8 @@ class CategoriesTest < ActiveSupport::TestCase
 
   test 'new membership category defaults to all wizard steps' do
     category = build_category()
+    category.save!
+
     assert_equal Effective::Applicant::WIZARD_STEPS.keys, Effective::Applicant.all_wizard_steps
     assert_equal Effective::Applicant.all_wizard_steps, category.applicant_wizard_steps
   end
