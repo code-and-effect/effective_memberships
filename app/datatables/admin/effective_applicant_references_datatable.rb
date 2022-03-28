0cfc452e9cac85cefab223f84a67c9a46f1ace78
Matt Riemer <matthew@codeandeffect.com>
2022-03-28 09:11:43 -0600

    use module admin

--- app/datatables/admin/effective_applicant_references_datatable.rb
+++ app/datatables/admin/effective_applicant_references_datatable.rb
@@ -1,31 +1,33 @@
-class Admin::EffectiveApplicantReferencesDatatable < Effective::Datatable
+module Admin
+  class EffectiveApplicantReferencesDatatable < Effective::Datatable
 
-  datatable do
-    order :name
+    datatable do
+      order :name
 
-    col :applicant
+      col :applicant
 
-    col :name
-    col :email
-    col :phone
+      col :name
+      col :email
+      col :phone
 
-    col :status do |reference|
-      if reference.submitted?
-        'Waiting on response'
-      elsif reference.completed?
-        'Completed'
+      col :status do |reference|
+        if reference.submitted?
+          'Waiting on response'
+        elsif reference.completed?
+          'Completed'
+        end
       end
+
+      col :last_notified_at do |reference|
+        reference.last_notified_at&.strftime('%F') unless reference.completed?
+      end
+
+      actions_col
     end
 
-    col :last_notified_at do |reference|
-      reference.last_notified_at&.strftime('%F') unless reference.completed?
+    collection do
+      Effective::ApplicantReference.deep.all
     end
 
-    actions_col
   end
-
-  collection do
-    Effective::ApplicantReference.deep.all
-  end
-
 end
